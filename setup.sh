544e8aecdd4884e059bbcc3831a183b95591a3dc
John Khoo <john_khoo@u.nus.edu>
2021-05-21 17:18:36 +0800

    Prepare for migration

---
+++ setup.sh
@@ -0,0 +1,6 @@
+# Core: syncthing, git
+# Languages: opam, ghc, go, guile, julia
+# Terminal: nnn + preview-tabbed, tabbed/suckless-tools, tmux, rg, fd[find]
+# Media: mpv, sxiv, zathura
+# Reading: qpdfview, okular
+# Hardware: ghdl, verilator, gtkwave, yosys
