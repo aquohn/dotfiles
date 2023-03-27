75f2cffc76d5a852099c45f1c4943b98b62cb136
John Khoo <john_khoo@u.nus.edu>
2023-03-27 19:38:50 +0800

    Remove sixel-tmux

--- software/sixel-tmux/config/.terminfo.src/config.sh
+++
@@ -1,7 +0,0 @@
-#Add the terminfo to .terminfo and /etc/terminfo:
-rm -fr $HOME/.terminfo/s $HOME/.terminfo/t $HOME/.terminfo/x
-
-for i in $HOME/.terminfo.src/*.terminfo ; do \
-        tic -xo $HOME/.terminfo $i ; 
-        tic -x $i ;  done
-
