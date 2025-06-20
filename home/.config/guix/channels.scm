7e3a38977fa8b46810eb786a91d5ce38cc02346e
John Khoo <johnkhootf@gmail.com>
2025-06-20 23:50:57 +0800

    Add guix-science-nonfree and guix-hpc

--- home/.config/guix/channels.scm
+++ home/.config/guix/channels.scm
@@ -31,5 +31,22 @@
           (make-channel-introduction
             "b1fe5aaff3ab48e798a4cce02f0212bc91f423dc"
             (openpgp-fingerprint
-              "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446")))))
+              "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
+      (channel
+        (name 'guix-science-nonfree)
+        (branch "master")
+        (url "https://codeberg.org/guix-science/guix-science-nonfree.git")
+        (introduction
+          (make-channel-introduction
+            "58661b110325fd5d9b40e6f0177cc486a615817e"
+            (openpgp-fingerprint
+              "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
+      (channel
+        (name 'guix-hpc)
+        (branch "master")
+        (url "https://gitlab.inria.fr/guix-hpc/guix-hpc.git"))
+      (channel
+        (name 'guix-hpc-non-free)
+        (branch "master")
+        (url "https://gitlab.inria.fr/guix-hpc/guix-hpc-non-free")))
 ;; %default-channels)
